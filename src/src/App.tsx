cf30b5d71fffc0d6f2dbfec3abae3c00bc5b0bd2
truongquanghoavugen <truongquanghoa.j415@gmail.com>
2026-01-02 18:17:51 +0700

    Update App.tsx

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,9 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+
 export default function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-600">
-        Fusion Frontend Ready 🚀
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
